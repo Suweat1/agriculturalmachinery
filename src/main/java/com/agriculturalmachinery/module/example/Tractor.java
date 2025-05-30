ada835058d3caf6d1c7fd9285bd1169711ebb461
ASUS <tel9649@qq.com>
2025-05-30 11:55:35 +0800

    更改了农机抽象信息类，创建了String格式化方法

--- src/main/java/com/agriculturalmachinery/module/example/Tractor.java
+++ src/main/java/com/agriculturalmachinery/module/example/Tractor.java
@@ -1,6 +1,7 @@
 package com.agriculturalmachinery.module.example;
 
 import com.agriculturalmachinery.module.AgriculturalMachineryInfo;
+import java.lang.String;
  /**
      * 定义了一些变量
      * 继承了AgriculturalMachineryInfo，并且重写其中的calculateRent方法，进行计算租金
@@ -37,7 +38,27 @@ public class Tractor extends AgriculturalMachineryInfo {
 
     /**
      * 开始
+     * 输入一个整型数 i
+     * 通过String.format方法
+     * 变成00i的形式，用0补齐
      * 把农机编号格式化成TR001 HV002这样的String格式
-     * 此类定义抽象方法
      */
-}
+    //重写实现formatMachineId方法
+     @Override
+     public String formatMachineId(int i){
+         String index = String.format("%03d", i);
+         String fomattedmachineId = getMachineId() + index;
+         return fomattedmachineId;
+     }
+
+     /**
+      * 开始
+      * 返回特征值
+      * 并格式化成：马力：i HP
+      */
+     //返回特征值的getProperValue方法的实现
+     @Override
+     public String getProperValue(){
+
+     }
+ }
