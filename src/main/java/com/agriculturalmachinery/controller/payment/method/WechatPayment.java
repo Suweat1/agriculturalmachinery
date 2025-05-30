28604711585750cc67822310c23dc44e7fdefa33
zzh <q274879556@163.com>
2025-05-30 10:18:50 +0800

    新增微信支付

---
+++ src/main/java/com/agriculturalmachinery/controller/payment/method/WechatPayment.java
@@ -0,0 +1,16 @@
+package com.agriculturalmachinery.controller.payment.method;
+
+import com.agriculturalmachinery.controller.payment.PaymentInfo;
+
+public class WechatPayment extends PaymentInfo {
+    private int OpenId;
+    public WechatPayment(double paymentAmount ,int OpenId) {
+        super(paymentAmount);
+
+        this.OpenId=OpenId;
+    }
+    public boolean processPayment() {
+    System.out.println("正在通过微信支付金额:"+paymentAmount+"账号为："+OpenId);
+    return true;
+    }
+}
